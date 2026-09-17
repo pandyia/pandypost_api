b62bb74e11790eb192d2b5a209c6227189a6fb94
Gabriel Moreira <gabriel.souza@saude.ba.gov.br>
2026-09-16 22:42:17 -0300

    fix reverb

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -31,9 +31,9 @@ return [
 
         'reverb' => [
             'driver' => 'reverb',
-            'key' => env('REVERB_APP_KEY'),
-            'secret' => env('REVERB_APP_SECRET'),
-            'app_id' => env('REVERB_APP_ID'),
+            'key' => env('REVERB_APP_KEY', ''),
+            'secret' => env('REVERB_APP_SECRET', ''),
+            'app_id' => env('REVERB_APP_ID', ''),
             'options' => [
                 'host' => env('REVERB_SERVER_HOST', env('REVERB_HOST')),
                 'port' => env('REVERB_PORT', 443),
@@ -47,9 +47,9 @@ return [
 
         'pusher' => [
             'driver' => 'pusher',
-            'key' => env('PUSHER_APP_KEY'),
-            'secret' => env('PUSHER_APP_SECRET'),
-            'app_id' => env('PUSHER_APP_ID'),
+            'key' => env('PUSHER_APP_KEY', ''),
+            'secret' => env('PUSHER_APP_SECRET', ''),
+            'app_id' => env('PUSHER_APP_ID', ''),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'host' => env('PUSHER_HOST') ?: 'api-'.env('PUSHER_APP_CLUSTER', 'mt1').'.pusher.com',
